ff879d0134a6783ead2c3bac895b7a5b0064e008
marin <rashi.arora@softage.ai>
2024-08-17 14:25:45 +0530

    Create project1.sql
    table creation in sql

---
+++ project1.sql
@@ -0,0 +1,11 @@
+-- data types in sql
+--create table table_name(
+ -- column_1 data_type,
+  --column-2 data_type,
+ -- column_3 data_type
+ -- );
+create table students(
+  roll_no INT,
+  name VARCHAR[20],
+  class VARCHAR[1]
+  );
