48c0873941063d5bbc5c36f1eda325314b71495d
marin <rashi.arora@softage.ai>
2024-08-17 13:04:13 +0530

    Update first.cs
    first code in c#

--- first.cs
+++ first.cs
@@ -1,1 +1,12 @@
 
+using system;
+namespace Helloworld
+{
+  class Program
+  {
+    static void main(string[] args)
+    {
+      Console.WriteLine("hello world!);
+                        }
+                        }
+                        }
